6e06c3a97e09d65858393e51f35e7f232864f50d
Gabe Aguilar <gabe.p.aguilar@gmail.com>
2020-10-20 15:25:45 -0500

    Update cash_register.rb

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
 
+class Cash_register
+
+    attr_accessor :total, :discount, :last_transaction_amount, :items
+
+    def initialize(total)
+        @total = 0
+        @discount = discount
+        @items = items 
+    end 
+
+    def add_item(title, price, quantity = 1)
+        if quantity > 1
+            i = 0
+            while i < quantity 
+                @items << title
+                i += 1
+            end 
+        else 
+            @items << title
+        end
+        @total += price*quantity
+        @last last_transaction_amount = @total
+        @total
+    end 
+
+    def apply_discount ()
+        if @discount > 0
+            @discount = @discount/100.to_f
+            @last_transaction_amount = @total
+            @total
+        end 
+
+
+
+
+end 
